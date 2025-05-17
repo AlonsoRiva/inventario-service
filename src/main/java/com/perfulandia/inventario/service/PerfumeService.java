3ea65ea5bab628933f7aefdd1e46606b4157e0a0
AlonsoRiva <al.rivadeneira@duocuc.cl>
2025-05-17 13:16:11 -0400

    Clase Service

--- src/main/java/com/perfulandia/inventario/service/PerfumeService.java
+++ src/main/java/com/perfulandia/inventario/service/PerfumeService.java
@@ -1,5 +1,42 @@
 package com.perfulandia.inventario.service;
 
+import com.perfulandia.inventario.model.Perfume;
+import com.perfulandia.inventario.repository.PerfumeRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class PerfumeService {
 
+    @Autowired
+    private PerfumeRepository perfumeRepository;
+
+    public List<Perfume> findAll() {
+        return perfumeRepository.findAll();
+    }
+
+    public Perfume findById(Long id) {
+        return perfumeRepository.findById(id).orElse(null);
+    }
+
+    public Perfume save(Perfume perfume) {
+        return perfumeRepository.save(perfume);
+    }
+
+    public void delete(Long id) {
+        perfumeRepository.deleteById(id);
+    }
+
+    public Perfume actualizarStock(Long id, int nuevoStock) {
+        Perfume perfume = perfumeRepository.findById(id).orElse(null);
+        if (perfume != null) {
+            perfume.setStock(nuevoStock);
+            return perfumeRepository.save(perfume);
+        }
+        return null;
+    }
 }
