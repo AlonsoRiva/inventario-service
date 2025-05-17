6c470ba7621a0528130b8c38c9891dd367eac0ed
AlonsoRiva <al.rivadeneira@duocuc.cl>
2025-05-17 13:13:11 -0400

    Clase perfume

--- src/main/java/com/perfulandia/inventario/model/Perfume.java
+++ src/main/java/com/perfulandia/inventario/model/Perfume.java
@@ -1,5 +1,18 @@
 package com.perfulandia.inventario.model;
 
-public class Perfume {
+import jakarta.persistence.*;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "perfumes")
+public class Perfume {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nombre;
+    private String marca;
+    private double precio;
+    private int stock;
 }
